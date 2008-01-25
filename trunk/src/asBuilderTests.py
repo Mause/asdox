97070289e667ffaa4e641bc33534fef5b5ecb387
michael_ramirez44@yahoo.com <michael_ramirez44@yahoo.com@ea85aa49-7e44-0410-bdef-df49d429eb4f>
2008-01-25 04:43:04 +0000

    Add basic interface test

--- trunk/src/asBuilderTests.py
+++ trunk/src/asBuilderTests.py
@@ -15,5 +15,20 @@ class ClassDefinitionTestCase(unittest.TestCase):
 		self.assertEqual(cls.methods,set())
 		self.assertEqual(cls.implements,set())
 		self.assertEqual(cls.includes,set())
+	def testBasicInterface(self):
+		cls = asModel.ClassDef("com.gurufaction.TestClass")
+		cls.type = "interface"
+		self.assertEqual(cls.name,"com.gurufaction.TestClass")
+		self.assertEqual(cls.type,"interface""Class not of type 'interface'")
+		self.assertEqual(cls.isInterface(),True,"isInterface() method failed")
+		self.assertEqual(cls.isDynamic(),False)
+		self.assertEqual(cls.isFinal(),False)
+		self.assertEqual(cls.isPublic(),False)
+		self.assertEqual(cls.extends,"Object")
+		self.assertEqual(cls.methods,set())
+		self.assertEqual(cls.variables,set())
+		self.assertEqual(cls.methods,set())
+		self.assertEqual(cls.implements,set())
+		self.assertEqual(cls.includes,set())
 if __name__ == "__main__":
 	unittest.main()
