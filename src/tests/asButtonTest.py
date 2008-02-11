d65ebf6f827186224304c97ccc57fb215ede3605
robert_ramirez44 <robert_ramirez44@yahoo.com>
2008-02-11 17:39:14 +0000

    added namespace test added metadata test

--- src/tests/asButtonTest.py
+++ src/tests/asButtonTest.py
@@ -87,7 +87,68 @@ class ASButtonTestCase(unittest.TestCase):
    
       """)
    
-      self.assertEqual(len(self.builder.getPackage("").getImports()),26,"Unable to parse import statements "+" Expectected 26 Returned: "+str(len(self.builder.getPackage("").getImports())))
+      self.assertEqual(self.builder.getPackage("").getName(),"","Unable to parse unnamed package")
+      self.assertEqual(len(self.builder.getPackage("").getImports()),26,"Unable to parse import statements "+" Expected 26 Returned: "+str(len(self.builder.getPackage("").getImports())))
+   
+   def testNamespace(self):
+      
+      self.builder.addSource(""" 
+      
+      package {
+
+      import flash.display.DisplayObject;
+      import flash.events.Event;
+      import flash.events.FocusEvent;
+      import flash.events.KeyboardEvent;
+      import flash.events.MouseEvent;
+      import flash.events.TimerEvent;
+      import flash.text.TextLineMetrics;
+      import flash.ui.Keyboard;
+      import flash.utils.Timer;
+      import mx.controls.dataGridClasses.DataGridListData;
+      import mx.controls.listClasses.BaseListData;
+      import mx.controls.listClasses.IDropInListItemRenderer;
+      import mx.controls.listClasses.IListItemRenderer;
+      import mx.core.EdgeMetrics;
+      import mx.core.IDataRenderer;
+      import mx.core.IFlexDisplayObject;
+      import mx.core.IInvalidating;
+      import mx.core.IUIComponent;
+      import mx.core.UIComponent;
+      import mx.core.UITextField;
+      import mx.core.mx_internal;
+      import mx.events.FlexEvent;
+      import mx.managers.IFocusManagerComponent;
+      import mx.skins.ProgrammaticSkin;
+      import mx.skins.RectangularBorder;
+      import mx.styles.ISimpleStyleClient;
+   
+      use namespace mx_internal;
+      
+      }      
+      
+      """)
+   
+      self.assertEqual(self.builder.getPackage("").getName(),"","Unable to parse unnamed package. Expected '' Returned: " + str(self.builder.getPackage("").getName()))
+      assertEqual(self.builder.getPackage("").getNamespaces(),1,"Unable to parse namespaces. Expected: 1 Returned" + str(len(self.builder.getPackage("").getNamespaces())))
+      
+      
+   def testMetaData(self):
+      
+      self.builder.addSource(""" 
+      
+      package {
+      
+      [Event(name="buttonDown", type="mx.events.FlexEvent")]
+      [Event(name="change", type="flash.events.Event")]
+      [Event(name="dataChange", type="mx.events.FlexEvent")]
+      
+      }
+      
+      """)
+  
+      self.assertEqual(self.builder.getPackage("").getName(),"","Unable to parse unnamed package. Expected '' Returned: " + str(self.builder.getPackage("").getName()))
+      self.assertEqual(len(self.getPackage("").getMetatags()),3,"Unable to parse metatags Expected: 3 Returned: " + str(self.getPackage("").getMetatags()))
       
 #run at commandline
 if __name__ == '__main__':
