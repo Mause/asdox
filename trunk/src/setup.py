d93b37c9df4edac16f69c9100122e1736394bc7a
michael_ramirez44@yahoo.com <michael_ramirez44@yahoo.com@ea85aa49-7e44-0410-bdef-df49d429eb4f>
2008-02-13 14:44:25 +0000

    Updated long description. Add version number.

--- trunk/src/setup.py
+++ trunk/src/setup.py
@@ -1,9 +1,8 @@
-longdesc = '''
-asDox is an Actionscript 3 parser written in Python. It is based on qDox for Java.
-
-The parser skims the source files only looking for things of interest such as class/interface definitions, import statements, JavaDoc tags and member declarations. The parser ignores things such as actual method implementations to avoid overhead.
-
-The end result of the parser is a very simple document model containing enough information to be useful.
+longdesc = '''asDox is an Actionscript 3 parser written in Python. 
+It is based on qDox project for Java. asDox parser 
+skims the source files only looking for things of 
+interest such as class/interface definitions, import 
+statements, JavaDoc tags and member declarations.
 '''
 
 # if someday we want to *require* setuptools, uncomment this:
@@ -22,7 +21,7 @@ except ImportError:
     kw = {}
 
 setup(name = "asdox",
-      version = "0.1",
+      version = "1.0-alpha",
       description = "Actionscript 3 Parser",
       author = "Michael Ramirez",
       author_email = "michael_ramirez44@yahoo.com",
@@ -31,7 +30,7 @@ setup(name = "asdox",
       download_url = 'http://code.google.com/p/asdox/downloads/list',
       license = 'New BSD License',
       platforms = 'All',
-      classifiers = [ 'Development Status :: 0.1 - Alpha',
+      classifiers = [ 'Development Status :: 1.0 - Alpha',
                       'Intended Audience :: Developers',
                       'License :: OSI Approved :: New BSD License',
                       'Operating System :: OS Independent'],
