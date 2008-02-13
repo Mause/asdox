f82fea4624e3148348dc26dd41da0e9f9dc25094
robert_ramirez44 <robert_ramirez44@yahoo.com>
2008-02-13 21:26:57 +0000

    remove class modifier test

--- src/tests/asBuilderTests.py
+++ src/tests/asBuilderTests.py
@@ -405,69 +405,32 @@ class ASFieldTestCase(BaseTestCase):
 		#test that 'today' field is of type int
 		self.assertEqual(pkg.getClass("MyClass").getField("count").getType(),"int","field was not of type int")
 	
-	def testClassModifiers(self):
+	def testFieldModifiers(self):
 		"Parse class modifier"
 		self.builder.addSource(""" 
 		package
 		{
 		        class MyClass
 			{
-			
+			  public var name:String;
+			  private var age:int;
+			  protected var salary:Number;
+			  
 			}
 			
-			public class MyClass1
-			{
-			
-			}
-			
-			dynamic class MyClass2
-			{
-			
-			}
-			
-			final class MyClass3
-			{
-			
-			}
-			
-			dynamic final class MyClass4
-			{
-			
-			}
 		}
 		""")
 		#test for valid package
-		self.assertEqual(self.builder.hasPackage(""),True,"Unnamed package not found in testStaticClassField")
+		#self.assertEqual(self.builder.hasPackage(""),True,"Unnamed package not found in testStaticClassField")
 		#get unnamed package
 		pkg = self.builder.getPackage("")
 		#test that a class has been declared
-		self.assertEqual(pkg.hasClass("MyClass"),True,"Package does not have one or more classes defined")
-		self.assertEqual(pkg.hasClass("MyClass1"),True,"Package does not have one or more classes defined")
-		self.assertEqual(pkg.hasClass("MyClass2"),True,"Package does not have one or more classes defined")
-		self.assertEqual(pkg.hasClass("MyClass3"),True,"Package does not have one or more classes defined")
-		self.assertEqual(pkg.hasClass("MyClass4"),True,"Package does not have one or more classes defined")
+		#self.assertEqual(pkg.hasClass("MyClass"),True,"Package does not have one or more classes defined")
 		# get class 'MyClass'
 		cls = pkg.getClass("MyClass")
-		# get class 'MyClass1'
-		cls1 = pkg.getClass("MyClass1")
-		# get class 'MyClass2'
-		cls2 = pkg.getClass("MyClass2")
-		# get class 'MyClass3'
-		cls3 = pkg.getClass("MyClass3")
-		# get class 'MyClass4'
-		cls4 = pkg.getClass("MyClass4")
 		#test for 'internal' class modifier
-		assertEqual(cls.hasModifier("internal"),True,"No internal class modifier found")
-		#test for public class modifer
-		assertEqual(cls1.hasModifier("public"),True,"No public class modifier found")
-		#test for dynamic class modifier
-		assertEqual(cls2.hasModifier("dynamic"),True,"No dynamic class modifer found")
-		#test for final class modifier
-		assertEqual(cls3.hasModifier("final"),True,"No final class modifer found")
-		#test for dynamic class modifier
-		assertEqual(cls4.hasModifier("dynamic"),True,"No dynamic class modifer found")
-		#test for final class modifer
-		assertEqual(cls4.hasModifier("final"),True,"No final class modifier found")
+		
+
 		
 	def testJavaDocWithClassFields(self):
 		pass
