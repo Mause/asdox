68a2fc65497e21a83ddb9714a4a26693538236da
michael_ramirez44@yahoo.com <michael_ramirez44@yahoo.com@ea85aa49-7e44-0410-bdef-df49d429eb4f>
2008-02-15 20:47:25 +0000

    Updated example to use getters/setters.

--- trunk/docs/examples/cheetah.py
+++ trunk/docs/examples/cheetah.py
@@ -7,8 +7,25 @@ package com.gurufaction.myApp
 {
 	public class MyClass
         {
-        	public var name:String;
-                public var age:int;
+        	private var _name:String;
+                private var _age:int;
+                
+                public function get name():String
+                {
+                	return _name;
+                }
+                
+                public function set name(value:String):void
+                {
+                	_name = value;
+                }
+                
+                // Age Property is Read-Only
+                public function get age():int
+                {
+                	return _age;
+                }
+                
         }
 }
 """)
@@ -20,13 +37,20 @@ using System;
 
 namespace $pkg.getName()
 {
-	#for $key,$value in $pkg.getClasses().items()
-        #if $value.hasModifier("public")
-        public class $value.getName()
+	#for $key,$cls in $pkg.getClasses().items()
+        #if $cls.hasModifier("public")
+        public class $cls.getName()
         {
-        	#for $key,$value in $value.getFields().items()
+        	#for $key,$value in $cls.getGetters().items()
                 #if $value.hasModifier("public")
-                public $value.getType() $value.getName();
+                public $value.getType() $value.getName()
+                {
+                        get { return _$value.getName().lower(); }
+                        #if $cls.hasSetter($value.getName())
+                        set {  _$value.getName().lower() = value; }   
+                        #end if
+                }
+                
                 #end if
                 #end for
         }
